400ad92ae01894ff91ff9e409b0c9009a0b035fb
vibhor <goyal1992.vibhor@gmail.com>
2021-07-02 19:50:29 +0530

    Added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,9 @@
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 
 class HelloAPIView(APIView):
@@ -40,3 +42,44 @@ class HelloAPIView(APIView):
         """Delete an object"""
         return Response({'method': 'DELETE'})
 
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test a viewset"""
+    serializer_class = serializers.HelloSerializer
+    
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            'Users actions (list, crate, retrieve. update, partial update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data['name']
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its id"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk = None):
+        """Handle removing a object"""
+        return Response({'http_method': 'DELETE'})
